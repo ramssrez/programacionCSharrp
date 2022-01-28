1366bf63c0052a2c7a81234dab8407761971107d
Raul Ramirez Perez <ramssrez@gmail.com>
2022-01-28 17:52:49 +0100

    Creación de la clase y el constructor

--- ProgramacionNet1/DPRN1_U1_A3_RARP/Program.cs
+++ ProgramacionNet1/DPRN1_U1_A3_RARP/Program.cs
@@ -1,5 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-string nombre = "Raúl Ramírez Pérez";
-string cuenta = "ES1921023412";
-Console.WriteLine(nombre + cuenta);
+﻿using System;
+namespace UnidadUnoA3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenidos a C#");
+            Presentacion presentacion = new Presentacion();
+            presentacion.ToString();
+        }
+    }
+
+    class Presentacion
+    {
+        public Presentacion()
+        {
+            //String nombre = "Nombre del autor, matrícula y título de la actividad así como la fecha y hora actual";
+            string nombre = "Raúl Ramírez Pérez";
+            string matricula = "ES1921023412";
+            string actividad = "Actividad 3 Resolver problemas con C#";
+
+            Console.WriteLine("Bienvenido a la actividad número 3 de esta unidad");
+            Console.WriteLine($"Programa desarrollado por: {nombre}");
+            Console.WriteLine($"Matricula: {matricula}");
+            Console.WriteLine($"Actividad: {actividad}");
+            Console.WriteLine($"Fecha del sistema: {DateTime.Now}");
+
+        }
+
+    }
+
+}
