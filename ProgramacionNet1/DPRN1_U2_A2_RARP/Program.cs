4ff4c9c608d962712d12e192aba6a7f2921b83a1
Raul Ramirez Perez <ramssrez@gmail.com>
2022-02-15 11:47:41 +0100

    codificación de los métodos de impresión

--- ProgramacionNet1/DPRN1_U2_A2_RARP/Program.cs
+++ ProgramacionNet1/DPRN1_U2_A2_RARP/Program.cs
@@ -9,25 +9,40 @@ namespace UnidadDosA2
         {
             //Declaración de variables
             string [] ciudades = new string[3];
-            char sexo;
-            double salario;
+            int [] numeros = new int[3];
             //Impresión y solicitud de datos 
             Console.WriteLine("**********************************************************************************************");
             Console.WriteLine("Bienvenidos al sistema de vacunación, ingresa los siguientes datos del paciente");
-            Console.Write("Ingresa el nombre del paciente: ");
             
             for (int i = 0; i < ciudades.Length; i++)
             {
                 Console.Write($"Ingresa el nombre de la ciudad {i + 1}: ");
                 ciudades[i] = Console.ReadLine();
             }
-
-            foreach (string ciudad in ciudades)
+            
+            for (int i = 0; i < numeros.Length; i++)
             {
-                Console.WriteLine(ciudad);
+                Console.Write($"Ingresa el numero {i + 1}: ");
+                numeros[i] = int.Parse(Console.ReadLine());
             }
+            listaCiudad(ciudades);
+            listaNumeros(numeros);
+        }
 
+        public static void listaCiudad(string [] ciudades)
+        {
+            for (int i = 0; i < ciudades.Length; i++)
+            {
+                Console.WriteLine($"La ciudad {i + 1} es: {ciudades[i]}");
+            }
+        }
 
+        public static void listaNumeros(int[] numeros)
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine($"El número {i + 1} es: {numeros[i]}");
+            }
         }
 
     }
