ed257ed6de625ef420d742db1ded7d5c62099df8
Raul Ramirez Perez <ramssrez@gmail.com>
2022-02-15 11:59:25 +0100

    Inicio de la dicumentación del problema

--- ProgramacionNet1/DPRN1_U2_A2_RARP/Program.cs
+++ ProgramacionNet1/DPRN1_U2_A2_RARP/Program.cs
@@ -1,50 +1,55 @@
 ﻿using System;
 namespace UnidadDosA2
 {
-
     class Program
     {
         //Comienzo de la función principal en C#
         static void Main(string[] args)
         {
-            //Declaración de variables
+            //Declaración de variables necesarias para 
             string [] ciudades = new string[3];
             int [] numeros = new int[3];
-            //Impresión y solicitud de datos 
+            //Impresión y solicitud de datos para 
             Console.WriteLine("**********************************************************************************************");
-            Console.WriteLine("Bienvenidos al sistema de vacunación, ingresa los siguientes datos del paciente");
-            
+            Console.WriteLine("Bienvenidos al sistema para observar el funcionamiento de métodos que no regresan datos");
+            Console.WriteLine(" ");
+            Console.WriteLine("Ingresa los nombres de tres las ciudades");
             for (int i = 0; i < ciudades.Length; i++)
             {
                 Console.Write($"Ingresa el nombre de la ciudad {i + 1}: ");
                 ciudades[i] = Console.ReadLine();
             }
-            
+            Console.WriteLine(" ");
+            Console.WriteLine("Ingresa tres numeros enteros diferentes");
             for (int i = 0; i < numeros.Length; i++)
             {
-                Console.Write($"Ingresa el numero {i + 1}: ");
+                Console.Write($"Ingresa el número {i + 1}: ");
                 numeros[i] = int.Parse(Console.ReadLine());
             }
+            Console.WriteLine("**********************************************************************************************");
             listaCiudad(ciudades);
             listaNumeros(numeros);
         }
-
         public static void listaCiudad(string [] ciudades)
         {
+            Console.WriteLine("**********************************************************************************************");
+            Console.WriteLine("Las ciudades son las siguientes");
             for (int i = 0; i < ciudades.Length; i++)
             {
                 Console.WriteLine($"La ciudad {i + 1} es: {ciudades[i]}");
             }
+            Console.WriteLine("**********************************************************************************************");
         }
-
         public static void listaNumeros(int[] numeros)
         {
+            Console.WriteLine("**********************************************************************************************");
+            Console.WriteLine("Los numeros son los siguientes");
             for (int i = 0; i < numeros.Length; i++)
             {
                 Console.WriteLine($"El número {i + 1} es: {numeros[i]}");
             }
+            Console.WriteLine("**********************************************************************************************");
         }
 
     }
-
 }
