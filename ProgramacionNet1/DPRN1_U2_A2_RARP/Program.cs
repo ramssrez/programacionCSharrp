13f4387326721a3ea636411e7ac07d542d7fd65d
Raul Ramirez Perez <ramssrez@gmail.com>
2022-02-15 12:05:05 +0100

    Finalización del programa

--- ProgramacionNet1/DPRN1_U2_A2_RARP/Program.cs
+++ ProgramacionNet1/DPRN1_U2_A2_RARP/Program.cs
@@ -9,41 +9,49 @@ namespace UnidadDosA2
             //Declaración de variables necesarias para 
             string [] ciudades = new string[3];
             int [] numeros = new int[3];
-            //Impresión y solicitud de datos para 
+            //Impresión y solicitud de datos para el desarrollo del problema
             Console.WriteLine("**********************************************************************************************");
-            Console.WriteLine("Bienvenidos al sistema para observar el funcionamiento de métodos que no regresan datos");
+            Console.WriteLine("Bienvenidos al siguiente sistema para observar el funcionamiento de métodos que no regresan datos");
             Console.WriteLine(" ");
             Console.WriteLine("Ingresa los nombres de tres las ciudades");
+            //Ingreso de las ciudades dentro de un arreglo de tipo string
             for (int i = 0; i < ciudades.Length; i++)
             {
                 Console.Write($"Ingresa el nombre de la ciudad {i + 1}: ");
+                //Lectura de las ciudades que se ingresen en la consola
                 ciudades[i] = Console.ReadLine();
             }
             Console.WriteLine(" ");
             Console.WriteLine("Ingresa tres numeros enteros diferentes");
+            //Ingreso de los numeros dentro de un arreglo de tipo entero
             for (int i = 0; i < numeros.Length; i++)
             {
                 Console.Write($"Ingresa el número {i + 1}: ");
+                //Lectura de los numeros que se ingresen en la consola
                 numeros[i] = int.Parse(Console.ReadLine());
             }
-            Console.WriteLine("**********************************************************************************************");
+            //Llamado a los métodos para la impresión de los datos ingresados 
             listaCiudad(ciudades);
             listaNumeros(numeros);
         }
+        //Método que lista las ciudades de un arreglo, no retorna valores
         public static void listaCiudad(string [] ciudades)
         {
             Console.WriteLine("**********************************************************************************************");
             Console.WriteLine("Las ciudades son las siguientes");
+            //Uso de sentencia for para poder imprimir las ciudades
             for (int i = 0; i < ciudades.Length; i++)
             {
                 Console.WriteLine($"La ciudad {i + 1} es: {ciudades[i]}");
             }
             Console.WriteLine("**********************************************************************************************");
         }
+        //Método que lista los numeros de un arreglo, no retorna valores
         public static void listaNumeros(int[] numeros)
         {
             Console.WriteLine("**********************************************************************************************");
             Console.WriteLine("Los numeros son los siguientes");
+            //Uso de sentencia for para poder imprimir los numeros
             for (int i = 0; i < numeros.Length; i++)
             {
                 Console.WriteLine($"El número {i + 1} es: {numeros[i]}");
