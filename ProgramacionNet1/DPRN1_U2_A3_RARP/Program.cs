e0afac4694a7717452b940d4e6012c2c882dbf04
Raul Ramirez Perez <ramssrez@gmail.com>
2022-02-16 08:47:19 +0100

    Creación del ejercicio

---
+++ ProgramacionNet1/DPRN1_U2_A3_RARP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace UnidadDosA3
+{
+    //Inicio de de codificación del programa
+    class Program
+    {
+        //Comienzo de la función principal en C#
+        static void Main(string[] args)
+        {
+            double bas = 20;
+            double altura = 15;
+            double longitudUno = 19.3041477;
+            double latitudUno = -99.1068475;
+        }
+    }
+}
