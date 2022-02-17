79112141f58246e2c5b0747c40d1ebb321a53688
Raul Ramirez Perez <ramssrez@gmail.com>
2022-02-17 13:14:05 +0100

    Finalizaión del calculo del costo total

--- ProgramacionNet1/DPRN1_U2_A3_RARP/Program.cs
+++ ProgramacionNet1/DPRN1_U2_A3_RARP/Program.cs
@@ -11,24 +11,10 @@ namespace UnidadDosA3
             double Altura = 15;
             double LongitudUno = - 99.1068475;
             double LatitudUno = 19.3041477;
-            double LongitudDos = -98.1068475;
-            double LatitudDos = 18.3041477;
-            double RadioTierra = 6372.795477598;
-            double DeltaLongitud = LongitudDos - LongitudUno;
-            double DeltaLatitud = LatitudDos - LatitudUno;
-            double PastoNatural =1000.0 ;
+            double LongitudDos = -99.068316;
+            double LatitudDos = 19.462441;
+            double PastoNatural = 1000.0 ;
             double PastoArtificial = 3500.0;
-            Console.WriteLine($"Base: {Base}");
-            Console.WriteLine($"Altura: {Altura}");
-            Console.WriteLine($"Long 1: {LongitudUno}");
-            Console.WriteLine($"Long 2: {LongitudDos}");
-            Console.WriteLine($"Lat 1: {LatitudUno}");
-            Console.WriteLine($"Lat 2: {LatitudDos}");
-            Console.WriteLine($"Radio: {RadioTierra}");
-            Console.WriteLine($"Delta long: {CalculoDelta(LongitudUno,LongitudDos)}");
-            Console.WriteLine($"Delta lati: {CalculoDelta(LatitudUno,LatitudDos)}");
-            Console.WriteLine($"Area: {CalculoArea(Base, Altura)} [m*m]");
-            Console.WriteLine($"Distancia: {CalculoDistancia(LatitudUno,LatitudDos,LongitudUno,LongitudDos)}[m]");
             CostoTotal(PastoNatural,Base,Altura,LatitudUno,LatitudDos,LongitudUno,LongitudDos);
         }
         public static double CalculoDelta(double ValorUno, double ValorDos)
@@ -62,11 +48,25 @@ namespace UnidadDosA3
         public static void CostoTotal(double TipoPasto, double Base, double Longitud, double LatitudUno, double LatitudDos, double LongitudUno, double LongitudDos)
         {
             double CostoDistancia = 40.0;
-            double Area, Distancia, Costo;
+            double Area, Distancia, Costo, CostoPasto, CostoDistanciaTotal, CostoQuince, CostoCinco, CostoSumado;
             Area = CalculoArea(Base, Longitud);
-            Distancia = CalculoDistancia(LatitudUno, LatitudDos , LongitudUno, LongitudDos);
-            Costo = ((TipoPasto * Area)+(CostoDistancia*Distancia)) * (0.20);
-            Console.WriteLine($"El costo es: {Costo}");
+            CostoPasto = Math.Round(TipoPasto * Area);
+            Distancia = Math.Round(CalculoDistancia(LatitudUno, LatitudDos, LongitudUno, LongitudDos));
+
+            CostoDistanciaTotal = Math.Round(CostoDistancia * Distancia);
+
+            Costo = Math.Round(CostoPasto + CostoDistanciaTotal);
+            CostoQuince = Math.Round(Costo * 0.15);
+            CostoCinco = Math.Round(Costo * 0.05);
+            CostoSumado = Costo + CostoQuince + CostoCinco;
+
+            Console.WriteLine($"El costo del pasto fue: ${TipoPasto} por metro cuadrado");
+            Console.WriteLine($"La área fue {Area}[m^2] con un costo: ${CostoPasto}");
+            Console.WriteLine($"El costo del trasporte es ${CostoDistancia} y {Distancia}[m]: ${CostoDistanciaTotal}");
+            Console.WriteLine($"El costo de instalación del 15% es: ${CostoQuince}");
+            Console.WriteLine($"El costo del material de 5% es: ${CostoCinco}");
+            Console.WriteLine($"El costo total de instalación: ${CostoSumado}");
         }
     }
 }
+//
