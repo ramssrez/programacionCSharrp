fe5a6f18223f6be46b3a6d81543d67c6ddda5b4f
Raul Ramirez Perez <ramssrez@gmail.com>
2022-02-20 17:44:33 +0100

    Creación de la clase Cliente

--- ProgramacionNet1/DPRN1_U2_EA_RARP/Program.cs
+++ ProgramacionNet1/DPRN1_U2_EA_RARP/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace UnidadDosEA
+{
+    //Inicio de de codificación del programa
+    class Program
+    {
+        //Comienzo de la función principal en C#
+        static void Main(string[] args)
+        {
+            //Llamado el método que realiza la presentación del programa
+            //Presentacion();
+        }
+        public static void Presentacion()
+        {
+
+        }
+        public static void IngresoDatosUsuario()
+        {
+
+        }
+        public static void MostrarInformacionUsuario()
+        {
+
+        }
+        public static void RealizarDepositos()
+        {
+
+        }
+        public static void RealizarRetiros()
+        {
+
+        }
+        public static void MostrarBalance()
+        {
+
+        }
+    }
+    class Cliente
+    {
+        public string Nombre { get; set; }
+        public string Clave { get; set;}
+        public string  TipoCliente { get;}
+        public string TipoOperacion { get;}
+        public string Monto { get; set;}
+        public int CantidadDepositos { get; set; }
+        public int CantidadRetiros { get; set; }
+        public Cliente()
+        {
+
+        }
+        public Cliente(string nombre, string clave, string tipoCliente, int depositos, int retiros)
+        {
+            Nombre = nombre;
+            Clave = clave;
+            TipoCliente = tipoCliente;
+            CantidadDepositos = depositos;
+            CantidadRetiros = retiros;
+        }
+
+    }
+}
