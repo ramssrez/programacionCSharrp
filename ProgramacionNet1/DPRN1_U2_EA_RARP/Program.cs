e061252fd4c3e67f1b0d6b96b174754b438b7992
Raul Ramirez Perez <ramssrez@gmail.com>
2022-02-20 19:06:13 +0100

    Creaciión de los métodos para ingresar la información del cliente

--- ProgramacionNet1/DPRN1_U2_EA_RARP/Program.cs
+++ ProgramacionNet1/DPRN1_U2_EA_RARP/Program.cs
@@ -16,7 +16,13 @@ namespace UnidadDosEA
         }
         public static void IngresoDatosUsuario()
         {
-
+            Cliente cliente = new Cliente();
+            Console.WriteLine("Ingrese el nombre del cliente: ");
+            cliente.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese la clave del cliente: ");
+            cliente.Clave = Console.ReadLine();
+            Console.WriteLine("Ingrese el tipo de cliente (Standart, Priority, Premium): ");
+            cliente.Nombre = Console.ReadLine();
         }
         public static void MostrarInformacionUsuario()
         {
@@ -41,20 +47,31 @@ namespace UnidadDosEA
         public string Clave { get; set;}
         public string  TipoCliente { get;}
         public string TipoOperacion { get;}
-        public string Monto { get; set;}
+        public double Monto { get; set;}
         public int CantidadDepositos { get; set; }
         public int CantidadRetiros { get; set; }
         public Cliente()
         {
 
         }
-        public Cliente(string nombre, string clave, string tipoCliente, int depositos, int retiros)
+        public Cliente(string nombre, string clave, string tipoCliente, int depositos, int retiros, double monto)
         {
             Nombre = nombre;
             Clave = clave;
             TipoCliente = tipoCliente;
             CantidadDepositos = depositos;
             CantidadRetiros = retiros;
+            Monto = monto;
+        }
+        public void ToString()
+        {
+            Console.WriteLine("Los datos del cliente son los siguientes");
+            Console.WriteLine($"Nombre del cliente: {Nombre} \n" +
+                $"Clase: {Clave} \n" +
+                $"Tipo de cliente {TipoCliente} \n" +
+                $"Cantidad depositos permitidos: {CantidadDepositos} \n" +
+                $"Cantidad retiros permitidos: {CantidadRetiros} \n" +
+                $"Monto: {Monto}\n");
         }
 
     }
