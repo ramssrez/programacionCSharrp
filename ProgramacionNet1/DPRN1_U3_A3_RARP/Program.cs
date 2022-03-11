18b7fd02116fa65fdb1d239e3c46f8ad35e6248c
Raul Ramirez Perez <ramssrez@gmail.com>
2022-03-11 20:42:42 +0100

    Creación de la clase principal

--- ProgramacionNet1/DPRN1_U3_A3_RARP/Program.cs
+++ ProgramacionNet1/DPRN1_U3_A3_RARP/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace UnidadTresA3
+{
+    //Inicio de de codificación del programa
+    class Program
+    {
+        //Comienzo de la función principal en C#
+        static void Main(string[] args)
+        {
+            //Llamado el método que realiza la presentación del programa
+            Presentacion();
+        }
+        //Método que muestra el menú principal para poder realiar el llamado a los diferentes métodos
+        public static void Presentacion()
+        {
+            Console.WriteLine("Hola mundo");
+        }
+    }
+}
