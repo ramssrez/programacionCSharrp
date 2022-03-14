5eff5afa5c15c0f483d66403fa884b3b5ccb5a70
Raul Ramirez Perez <ramssrez@gmail.com>
2022-03-14 12:16:51 +0100

    Creación de los metodos para poder vaidar un numero y que sea mayor a cuatro

--- ProgramacionNet1/DPRN1_U3_A3_RARP/Program.cs
+++ ProgramacionNet1/DPRN1_U3_A3_RARP/Program.cs
@@ -8,8 +8,16 @@ namespace UnidadTresA3
         //Comienzo de la función principal en C#
         static void Main(string[] args)
         {
-            int fila = 10;
-            int columna = 10;
+            int fila = ValidarNumero("Ingresa el valor de la fila: ");
+            Console.WriteLine(fila);
+            int columna = ValidarNumero("Ingresa el valor de la columna: ");
+            Console.Write(columna);
+
+
+
+            /*
+            int fila = 5;
+            int columna = 5;
             string cara = "%";
             var input = "147812357898";
             var reg = @"\d{3}";
@@ -30,8 +38,10 @@ namespace UnidadTresA3
             ImprimirLetraL(fila, columna, matriz);
             Console.WriteLine();
             ImprimirLetraA(fila, columna, matriz);
+            Console.WriteLine();
+            */
 
-            PruebaTotal(10,10);
+            //PruebaTotal(4,5);
 
             //Llamado el método que realiza la presentación del programa
             //Presentacion();
@@ -39,8 +49,66 @@ namespace UnidadTresA3
         //Método que muestra el menú principal para poder realiar el llamado a los diferentes métodos
         public static void Presentacion()
         {
+            Console.WriteLine("**********************************************************************************************");
+            Console.WriteLine("Bienvenidos al sistema para imprimir LA con un tipo de caracter");
+            Console.Write("Ingresa el tamaño de la fila: ");
+            int fila = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingresa el tamaño de la columna: ");
+            int columna = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingresa el caracter a imprimir: ");
+            string caracter = Console.ReadLine();
 
+            //Varaible para salir del ciclo.
+            bool salir = false;
+            //Inicio del comienzo de ciclo para mostrar las diferentes opciones del menú
+            while (!salir)
+            {
+                //Impresión de los diferentes opciones
+                Console.WriteLine("1. Área y perímetro de un Trapecio");
+                Console.WriteLine("2. Área y perímetro de una Estrella de 6 puntas");
+                Console.WriteLine("3. Área y perímetro de un Círculo");
+                Console.WriteLine("4. Área y perímetro de una Estrella de 5 puntas");
+                Console.WriteLine("5. Salir");
+                Console.Write("Ingresa una opción: ");
+                int opcion = Convert.ToInt32(Console.ReadLine());
+                //Uso del switch para seleccion de las opciones ingresadas desde la consola
+                switch (opcion)
+                {
 
+                }
+            }
+        }
+        public static bool EsIgualMayorCuatro(int valor)
+        {
+            bool validacion = false;
+            if (valor >= 4)
+            {
+                validacion = true;
+            }
+            else
+            {
+                Console.WriteLine("Solicitud no valida, numero tiene que ser mayor a cuatro");
+            }
+            return validacion;
+        }
+        public static int ValidarNumero(string mensaje)
+        {
+            bool salir = false;
+            int valor = 0;
+            while (!salir)
+            {
+                try
+                {
+                    Console.Write(mensaje);
+                    valor = Convert.ToInt32(Console.ReadLine());
+                    salir = EsIgualMayorCuatro(valor);
+                }
+                catch (FormatException ex)
+                {
+                   Console.WriteLine("Error al ingresa valor, vuelve a intentarlo");
+                }
+            }
+            return valor;
         }
         public static void ImprimirLetraA(int fila, int columna, string[,] matriz)
         {
@@ -71,8 +139,7 @@ namespace UnidadTresA3
 
             }
         }
-
-            public static void ImprimirLetraL(int fila, int columna, string[,] matriz)
+        public static void ImprimirLetraL(int fila, int columna, string[,] matriz)
         {
             for (int f = 0; f < fila; f++)
             {
@@ -85,32 +152,12 @@ namespace UnidadTresA3
                     {
                         Console.Write(matriz[f, c]);
                     }
+                   
                 }
                 Console.WriteLine();
             }
         }
-        public static void PruebaTotal(int fila, int col)
-        {
-            int columaTotal = (col * 2) + 1;
-            string[,] matriz = IngresarValorVetor(fila,columaTotal,"&");
-            for (int f = 0; f < fila; f++)
-            {
-                for (int c = 0; c < columaTotal; c++)
-                {
-                    if (c == 0 && f <= fila)
-                    {
-                        Console.Write(matriz[f, c]);
-                    }
-                    else if (f == (fila - 1) && c > 0)
-                    {
-                        Console.Write(matriz[f, c]);
-                    }
-                }
-                Console.WriteLine();
-            }
-
-        }
-        public static string[,] IngresarValorVetor(int fila, int columna,string caracter)
+        public static string[,] IngresarValorVetor(int fila, int columna, string caracter)
         {
             string[,] ejemplo = new string[fila, columna];
             for (int f = 0; f < fila; f++)
@@ -122,6 +169,56 @@ namespace UnidadTresA3
             }
             return ejemplo;
         }
+        /*
+        public static void PruebaTotal(int fila, int col)
+        {
+            string espacio = "";
+            int columaTotal = (col * 2) + 1;
+            string[,] matriz = IngresarValorVetor(fila,columaTotal,"&");
+            for (int f = 0; f < fila; f++)
+            {
+                for (int c = 0; c < columaTotal; c++)
+                {
+                    if (c == 0 && f <= fila)
+                    {
+                        Console.Write(matriz[f, c]);
+                    }                    
+                    else if (f == (fila - 1) && c > 0 && c<=col-1)
+                    {
+                        Console.Write(matriz[f, c]);
+                    }
+                    
+                    else if (c == (col+1) && f < fila-1)
+                    {
+                        Console.Write(espacio.PadRight(col) + matriz[f, c]);
+                    }
+                    else if (c == (col + 1) && f <= fila)
+                    {
+                        Console.Write(matriz[f, c]);
+                    }
+                    
+                    else if (f == 0)
+                    {
+                        Console.Write(matriz[f, c]);
+                    }
+                    else if (f == 2)
+                    {
+                        Console.Write(matriz[f, c]);
+                    }
+                    else if (c == (columaTotal - 1) && f <= fila)
+                    {
+                        Console.Write(espacio.PadRight(columaTotal - 2) + matriz[f, c]);
+                    }
+                    
+
+                    //Console.Write("+");
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+        */
 
 
     }
