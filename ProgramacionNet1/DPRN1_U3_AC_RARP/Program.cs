b74226460801e3234f90cc324a92f25f3e999dc3
Raul Ramirez Perez <ramssrez@gmail.com>
2022-03-16 10:30:58 +0100

    Creación del pbjeto paciente y sus metodos para imprimirlo

--- ProgramacionNet1/DPRN1_U3_AC_RARP/Program.cs
+++ ProgramacionNet1/DPRN1_U3_AC_RARP/Program.cs
@@ -7,9 +7,18 @@ namespace UnidadTresEA
         //Comienzo de la función principal en C#
         static void Main(string[] args)
         {
+            string CURP = "RAPR920627HMCMRL02";
+            string cuatroCaracter = CURP.Substring(0, 4);
+            Console.WriteLine(cuatroCaracter);
+            /*
             string s = ValidarSoloString("Ingresa un string: ");
             Console.WriteLine("sdfsdfsdf: " +s);
             int opcion = ValidarNumero("Ingresa una opción del menú: ");
+            Console.WriteLine("sdfsdfsdf: " + opcion);
+            string ss = ValidarString("Ingresa un string: ");
+            Console.WriteLine("sdfsdfsdf: " + ss);
+            */
+
             //Llamado el método que realiza la presentación del programa
             //Presentacion();
         }
@@ -69,13 +78,11 @@ namespace UnidadTresEA
                 }
                 catch (FormatException ex)
                 {
-                    Console.WriteLine(ex.Message);
                     //Mensaje de error en caso de que no se haya ingresado un entero
                     Console.WriteLine("Error al ingresa valor, vuelve a intentarlo");
                 }
                 catch (OverflowException ex)
                 {
-                    Console.WriteLine(ex.Message);
                     //Mensaje de error en caso de que se haya ingresado un número muy grande
                     Console.WriteLine("Número demasiado grande, intenta con otro número");
                 }
@@ -95,17 +102,50 @@ namespace UnidadTresEA
                 s = Console.ReadLine();
                 if (!SoloLetras(s))
                 {
-                    Console.WriteLine("Ingresa solo letras en la sección, sin espacios");
+                    Console.WriteLine("Ingresa solo letras, sin espacios");
                 }else if (string.IsNullOrEmpty(s))
                 {
-                    Console.WriteLine("El string se encuentra vación, vuelve a intentarlo");
+                    Console.WriteLine("El string se encuentra vacío, vuelve a intentarlo");
                 }
                 else
                 {
                     salir = true;
                 }                
             }
+
             //Retorno del string que se ha ingresado
+            return s;
+        }
+        public static string ValidarString(string mensaje)
+        {
+            bool salir = false;
+            string s = null;
+            //Ciclo while para verificar las caracteristicas de un caracter
+            while (!salir)
+            {
+                try
+                {
+                    Console.Write(mensaje);
+                    s = Console.ReadLine();
+                    if (string.IsNullOrEmpty(s))
+                    {
+                        Console.WriteLine("El string se encuentra vacío, vuelve a intentarlo");
+                    }
+                    else if(EspacioVacio(s))
+                    {
+                        Console.WriteLine("Ingresa un string sin espacios");
+                    }
+                    else
+                    {
+                        salir = true;
+                    }
+
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Error al ingresar el string");
+                }
+            }
             return s;
         }
         //Metodo que permite identificar si los caracteres son letras o no
@@ -120,6 +160,153 @@ namespace UnidadTresEA
             }
             return true;
         }
+        static bool EspacioVacio(string sCaracteres)
+        {
+            foreach (char ch in sCaracteres)
+            {
+                if (ch != 32)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 
+    }
+
+    class Paciente
+    {
+        //Declaración de variables y métodos getter y setter del objeto PersonaRegistro
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string FechaNacimiento { get; set; }
+        public int Edad { get; set; }
+        public char Sexo { get; set; }
+        public string TipoSangre { get; set; }
+        public string FechaConsulta { get; set; }
+        public string MotivoConsulta { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string NumeroCelular { get; set; }
+        public string CURP { get; set; }
+        public string Folio { get; set; }
+        //Declaración de constructor vacío de la clase
+        Paciente()
+        {
+
+        }
+        //Declaración del constructor de la clase con los elementos que se registraron a partir de la consola
+        public Paciente(string nombre, string apellidoPaterno, string apellidoMaterno, string fechaNacimiento, 
+            char sexo, string correo, string tipoSangre, string fechaConsulta, string motivoConsulta, string numeroCelular, string curp)
+        {
+            //Asignación de los atributos de la clase con los datos que se ingresaron en la consola
+            Nombre = nombre;
+            ApellidoMaterno = apellidoMaterno;
+            ApellidoPaterno = apellidoPaterno;
+            FechaNacimiento = fechaNacimiento;
+            Sexo = sexo;
+            TipoSangre = tipoSangre;
+            FechaConsulta = fechaConsulta;
+            MotivoConsulta = motivoConsulta;
+            NumeroCelular = numeroCelular;
+            CURP = curp;
+            CorreoElectronico = correo;
+            //Asignación de atributos de la clase con métodos que se encuentran dentro de la clase
+            Edad = CalcularEdad(fechaNacimiento);
+            Folio = FolioVacunacion(nombre, apellidoPaterno, apellidoMaterno);
+        }
+        //Método que calcula la edad del paciente con la diferencia de fechas, la fecha actual y la fecha de nacimientos
+        private int CalcularEdad(string fechaNacimiento)
+        {
+            //Determinación de la fecha actual
+            DateTime dateNow = DateTime.Now;
+            //Parseo de la fecha de nacimiento a un tipo DateTime
+            DateTime dateNacimiento = DateTime.Parse(fechaNacimiento);
+
+            //Obtención del año, meses y dias de la resta entre las fechas
+            int year = dateNow.Year - dateNacimiento.Year;
+            int month = dateNow.Month - dateNacimiento.Month;
+            int day = dateNow.Day - dateNacimiento.Day;
+            //Comparación de los meses para determinar el número de años con if/else
+            if (month < 0)
+            {
+                //Retorno de la edad en años, cuando la diferencia de meses es menor a cero
+                return year - 1;
+            }
+            //else if para el caso de que los meses sean igual a cero, se verifica la diferencia entre los días
+            else if (month == 0)
+            {
+                //En caso de que los dias son mayores o igual a cero se retorna el año
+                if (day >= 0)
+                {
+                    return year;
+                }
+                else
+                {
+                    //En caso contrario se le resta un -1 al año
+                    return year - 1;
+                }
+            }
+            //En caso de las ultimas dos opciones no fueran correctas se retorna el año
+            else
+            {
+                return year;
+            }
+        }
+        //Método que permite generar un número aleatorio entre 0 al 1000
+        private int RandomNumero()
+        {
+            //Declaración de la clase Random
+            Random random = new Random();
+            //Declaración del valor a retornar y el rango para generarlos 
+            int valor = random.Next(0, 1001);
+            //Retorno del valor generado
+            return valor;
+        }
+        //Método que permite seleccionar una letra random de un arreglo
+        private char RandomLetra()
+        {
+            //Declaración de la clase Random
+            Random random = new Random();
+            //Declaración del arreglo que contiene las letras del abcedario que vamos a utilizar
+            char[] letras = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            //Selección de las letras random
+            char letra = letras[random.Next(0, 26)];
+            //Retorno de la letra seleccioanda
+            return letra;
+        }
+        //Método que permite generar el folio de vacunación del paciente, en función de su nombre y apellidos
+        private string FolioVacunacion(string nombre, string apellidoPaterno, string apellidoMaterno)
+        {
+            string folio;
+            //Selección de la primera letra del nombre
+            char letraNombre = nombre[0];
+            //Seleccion de la segunda letra del apellido paterno
+            char letraPaterno = apellidoPaterno[1];
+            //Selección de la tercera letra del apellido materno
+            char letraMat = apellidoMaterno[2];
+            //Generación del folio, usando métodos para pasar a mayusculas las letras seleccionadas y los métodos random
+            folio = $"{Char.ToUpper(letraNombre)}{Char.ToUpper(letraPaterno)}{Char.ToUpper(letraMat)}-{RandomNumero()}-{RandomLetra()}";
+            //Retorno del folio generado
+            return folio;
+        }
+        //Método que permite imprimir los datos de la clase con formato para este caso en especifico
+        public void ToString()
+        {
+            //Console.WriteLine("Los datos del paciente e historial de vacunación");
+            Console.WriteLine($"Nombre del paciente: {Nombre} \n" +
+                $"Apellido Paterno: {ApellidoPaterno} \n" +
+                $"Apellido Materno {ApellidoMaterno} \n" +
+                $"Fecha de Nacimiento: {FechaNacimiento} \n" +
+                $"Sexo: {Sexo} \n" +
+                $"Edad: {Edad}\n" +
+                $"Correo: {CorreoElectronico} \n" +
+                $"Tipo de Sangre: {TipoSangre} \n" +
+                $"Fecha Consulta: ${FechaConsulta} \n" +
+                $"Motivo Consulta: {MotivoConsulta} \n" +
+                $"Número Celular: {NumeroCelular} \n" +
+                $"CURP: {CURP} \n" +
+                $"Folio: {Folio} \n");
+        }
     }
 }
