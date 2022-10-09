a730a886d0472881cfa9187e15d4728746883838
Raul Ramirez Perez <ramssrez@gmail.com>
2022-10-09 12:06:27 +0200

    Creación del la actividad número uno de la unidad uno

---
+++ ProgramaciónNet2/DPRN2_U1_A1_RARP/Personaje.cs
@@ -0,0 +1,27 @@
+﻿namespace UnidadUnoEA
+{
+    public class Personaje
+    {
+        public string NombreReal { get; set; }
+        public string NombreConocido { get; set; }
+        public string PrimeraAparicionHistorica { get; set; }
+        public string PrimeraAparicion { get; set; }
+        public string Poderes { get; set; }
+        public string Creador { get; set; }
+        public string Descripcion { get; set; }
+        public bool Humano { get; set; }
+        public string PuntosExperiencia { get; set; }
+        public string NivelFisico { get; set; }
+
+
+        public Personaje()
+        {
+
+        }
+        Personaje(string nombreConocido, string nombreReal, string primeraAparicionHistorica,
+            string primeraAparicion, string poderes, string creador, string descripcion)
+        {
+
+        }
+    }
+}
---
+++ ProgramaciónNet2/DPRN2_U1_A1_RARP/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace UnidadUnoEA
+{
+    class Program
+    {
+        //Comienzo de la función principal en C#
+        static void Main(string[] args)
+        {
+            Personaje personaje = new Personaje();
+            personaje.NombreReal = "Diana";
+            Console.WriteLine("Hola mundo" + personaje.NombreReal);
+            Console.WriteLine("**********************************************************************************************");
+        }
+    }
+    /*
+    class Personaje
+    {
+        public string NombreReal { get; set; }
+        public string NombreConocido { get; set; }
+        public string PrimeraAparicionHistorica { get; set; }
+        public string PrimeraAparicion { get; set; }
+        public string Poderes { get; set; }
+        public string Creador { get; set; }
+        public string Descripcion { get; set; }
+        public bool Humano { get; set; }
+        public string PuntosExperiencia { get; set; }
+        public string NivelFisico { get; set; }
+
+
+        Personaje()
+        {
+
+        }
+        Personaje(string nombreConocido, string nombreReal, string primeraAparicionHistorica,
+            string primeraAparicion, string poderes, string creador, string descripcion)
+        {
+
+        }
+    }
+    */
+}
