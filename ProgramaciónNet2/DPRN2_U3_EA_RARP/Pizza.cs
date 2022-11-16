2cc6d5bfa56b1ba2f9dd7682121267f6c9e9a4c8
Raul Ramirez Perez <ramssrez@gmail.com>
2022-11-15 21:04:33 -0600

    Cración de las clases con sus respectivos atributos

--- ProgramaciónNet2/DPRN2_U3_EA_RARP/Pizza.cs
+++ ProgramaciónNet2/DPRN2_U3_EA_RARP/Pizza.cs
@@ -3,12 +3,36 @@
     class Pizza
     {
         public List<Ingrediente> Ingredientes { get; set; }
+        public int Id { get; set; }
         public string PizzaName { get; set; }
         public Tipo Type { get; set; }
         public string Tiempo { get; set; }
+        public static int contador = 1;
+
         public Pizza()
         {
 
+        }
+        public Pizza(string name, List<Ingrediente> ingredientes, Tipo tipo, string tiempo)
+        {
+            Ingredientes = ingredientes;
+            PizzaName =name;
+            Type = tipo;
+            Tiempo = tiempo;
+            Id = contador++;
+        }
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Id: {Id} \n" +
+                $"Nombre: {PizzaName}\n" +
+                $"Tiempo de cocción: {Tiempo} minutos\n" +
+                $"Tamaño: {Type.Name}\n" +
+                $"Precio: ${Type.Precio}");
+            for (int i=0; i<Ingredientes.Count;i++)
+            {
+                Console.WriteLine($"{i+1} - {Ingredientes[i].Name}");
+            }
+            Console.WriteLine($"\n");
         }
     }
 }
--- ProgramaciónNet2/DPRN2_U3_EA_RARP/Program.cs
+++ ProgramaciónNet2/DPRN2_U3_EA_RARP/Program.cs
@@ -2,6 +2,8 @@
 {
     class Program
     {
+        public static List<Ingrediente> ingredientes;
+        public static List<Pizza> pizzas;
         static void Main(string[] args)
         {
             //Inicio del método que contiene el menú de principal.
@@ -10,7 +12,37 @@
 
         private static void Presentacion()
         {
-            List<Ingrediente> ingredientes = new List<Ingrediente>();
+            CrearIngredientes();
+            CrearPizzas();
+            //throw new NotImplementedException();
+        }
+
+        private static void CrearPizzas()
+        {
+            pizzas = new List<Pizza>();
+            Pizza pizza = new("Carlos especial", new List<Ingrediente> { new Ingrediente("Secretos")}, new Tipo("Chica", 100.0), "30");
+            Pizza pizza1 = new("Carlos especial", new List<Ingrediente> { new Ingrediente("Secretos") }, new Tipo("Median", 150.0), "30");
+            Pizza pizza2= new("Carlos especial", new List<Ingrediente> { new Ingrediente("Secretos") }, new Tipo("Grande", 200.0), "30");
+
+            Pizza pizza3 = new("Hawaiana", new List<Ingrediente> { ingredientes[0],ingredientes[1],ingredientes[2],ingredientes[3]},new Tipo("Chica",100.0),"28");
+            Pizza pizza4 = new("Hawaiana", new List<Ingrediente> { ingredientes[0], ingredientes[1], ingredientes[2], ingredientes[3] }, new Tipo("Mediana", 150.0), "28");
+            Pizza pizza5 = new("Hawaiana", new List<Ingrediente> { ingredientes[0], ingredientes[1], ingredientes[2], ingredientes[3] }, new Tipo("Grande", 200.0), "28");
+            pizzas.Add(pizza);
+            pizzas.Add(pizza1);
+            pizzas.Add(pizza2);
+            pizzas.Add(pizza3);
+            pizzas.Add(pizza4);
+            pizzas.Add(pizza5);
+            foreach (Pizza p in pizzas)
+            {
+                p.MostrarInfo();
+            }
+            //throw new NotImplementedException();
+        }
+
+        private static void CrearIngredientes()
+        {
+            ingredientes = new List<Ingrediente>();
             Ingrediente ing1 = new("Queso");
             Ingrediente ing2 = new("Jamón");
             Ingrediente ing3 = new("Tocino");
@@ -24,6 +56,7 @@
             Ingrediente ing11 = new("Anchoas");
             Ingrediente ing12 = new("Mariscos");
             Ingrediente ing13 = new("Peperoni");
+            //Ingrediente ing14 = new("Peperoni");
             ingredientes.Add(ing1);
             ingredientes.Add(ing2);
             ingredientes.Add(ing3);
@@ -39,9 +72,8 @@
             ingredientes.Add(ing13);
             foreach (Ingrediente i in ingredientes)
             {
-                i.MostrarInfo();
+                Console.WriteLine($"{i.Id} - {i.Name}");
             }
-            //throw new NotImplementedException();
         }
     }
 }
