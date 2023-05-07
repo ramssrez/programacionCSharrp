4bd8e7b18846cca320586f8557160170f9cfd66f
Raul Ramirez Perez <ramssrez@gmail.com>
2023-05-07 17:14:48 +0200

    Creación del proyecto de la actividad uno de la unidad dos

---
+++ ProgramacionNetII/DPRN2_U2_A1_RARP/Program.cs
@@ -0,0 +1,147 @@
+﻿namespace UnidadDosA1
+{
+    class Program
+    {
+        //Declaración de las variables globales
+        /*
+        public static Cartas? cartas;
+        public static List<Carta>? cartasList;
+        public static Carta? carta;
+        public static StringProyect strings;
+        */
+        //Comienzo de la función principal en C#
+        static void Main(string[] args)
+        {
+            //Instancia de las variables globales
+            //cartas = new Cartas();
+            //cartasList = cartas.CartasList;
+
+            //Llamado al método de presentación
+            Presentacion();
+        }
+        //Declaración del método presentación
+        public static void Presentacion()
+        {
+            Console.WriteLine("Esto es de las ");
+            // Mensaje de presentación del programa
+            Console.WriteLine(StringProyect.STRING_FORMATO);
+            Console.WriteLine(StringProyect.BIENVENIDO);
+            //Variable para salir del ciclo.
+            bool salir = false;
+            //Inicio del comienzo de ciclo para mostrar las diferentes opciones del menú
+            while (!salir)
+            {
+                //Impresión de los diferentes opciones
+                Console.WriteLine(StringProyect.OPCIONES_MENU);
+                int opcion = ValidarNumero(StringProyect.OPCION_MENU);
+                //Uso del switch para seleccion de las opciones ingresadas desde la consola
+                switch (opcion)
+                {
+                    //Caso para ingresar los datos del primer Iris
+                    case 1:
+                        Console.WriteLine(StringProyect.STRING_FORMATO);
+                        Console.WriteLine(StringProyect.OpcionMenu(opcion));
+                        //irisUno = CrearIris();
+                        Console.WriteLine(StringProyect.STRING_FORMATO);
+                        break;
+                    //Caso para ingresar los datos del segundo Iris
+                    case 2:
+                        Console.WriteLine(StringProyect.STRING_FORMATO);
+                        Console.WriteLine(StringProyect.OpcionMenu(opcion));
+                        //irisDos = CrearIris();
+                        Console.WriteLine(StringProyect.STRING_FORMATO);
+                        break;
+                    //Caso para poder mostrar la autentificación de los dos iris ingresados, así como el porcentaje de similitud
+                    case 3:
+                        Console.WriteLine(StringProyect.STRING_FORMATO);
+                        Console.WriteLine(StringProyect.OpcionMenu(opcion));
+                        /*
+                        if (irisUno != null && irisDos != null)
+                        {
+                            Reconocimiento re = new Reconocimiento(irisUno, irisDos);
+                            re.AtributosConsole();
+                        }
+                        else
+                        {
+                            Console.WriteLine(StringProyect.NO_SELECCION_IRIS);
+                        }
+                        */
+                        Console.WriteLine(StringProyect.STRING_FORMATO);
+                        break;
+                    case 4:
+                        //Caso para poder simular el envejecimiento de la iris
+                        Console.WriteLine(StringProyect.STRING_FORMATO);
+                        Console.WriteLine(StringProyect.OpcionMenu(opcion));
+                        /*
+                        if (irisUno != null && irisDos != null)
+                        {
+                            int opc = SeleccionIris();
+                            if (opc == 1)
+                            {
+                                SelecionEnvejecimiento(irisUno);
+                            }
+                            else if (opc == 2)
+                            {
+                                SelecionEnvejecimiento(irisDos);
+                            }
+                            else
+                            {
+                                Console.WriteLine(StringProyect.NO_SELECCION_SUBMENU);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine(StringProyect.NO_SELECCION_IRIS);
+                        }
+                        */
+                        Console.WriteLine(StringProyect.STRING_FORMATO);
+                        break;
+                    case 5:
+                        //Opción para la salida del programa
+                        Console.WriteLine(StringProyect.STRING_FORMATO);
+                        Console.WriteLine(StringProyect.SALIR);
+                        Console.WriteLine(StringProyect.STRING_FORMATO);
+                        salir = true;
+                        break;
+                    //Opción en el caso de que el usuario no seleccione una opción del menú
+                    default:
+                        Console.WriteLine(StringProyect.STRING_FORMATO);
+                        Console.WriteLine(StringProyect.RANGO_OPCIONES);
+                        Console.WriteLine(StringProyect.STRING_FORMATO);
+                        break;
+                }
+            }
+        }
+        //Método que varifica si es un entero el valor ingresado desde la consola, se repite hasta que sea correcto
+        public static int ValidarNumero(string mensaje)
+        {
+            //Declaración de variables necesarias para realizar el programa
+            bool salir = false;
+            int valor = 0;
+            //Ciclo while que se repite en caso de que no se haya  ingresado un entero o sea menor a cuatro
+            while (!salir)
+            {
+                //Uso de la sentencia try/catch para el caso de que no se ingrese un entero
+                try
+                {
+                    Console.Write(mensaje);
+                    valor = Convert.ToInt32(Console.ReadLine());
+                    //Llamado al metodo para verificar que el valor ingresado sea mayor a cuatro
+                    salir = true;
+                }
+                catch (FormatException ex)
+                {
+                    //Mensaje de error en caso de que no se haya ingresado un entero
+                    Console.WriteLine(StringProyect.ERROR_INGRESAR_NUMERO);
+                }
+                catch (OverflowException ex)
+                {
+                    //Mensaje de error en caso de que se haya ingresado un número muy grande
+                    Console.WriteLine(StringProyect.ERROR_NUMERO_GRANDE);
+                }
+            }
+            //Retorno del valor ingresado
+            return valor;
+        }
+    }
+}
---
+++ ProgramacionNetII/DPRN2_U2_A1_RARP/StringProyect.cs
@@ -0,0 +1,40 @@
+﻿namespace UnidadDosA1
+{
+    class StringProyect
+    {
+        //Atributos de la clase que contiene los strings a utilizar en el proyecto
+        public const string STRING_FORMATO = "**********************************************************************************************";
+        public const string BIENVENIDO = "Bienvenidos al sistema de reconocimiento de Iris";
+        public const string SALIR = "Has elegido salir de la aplicación";
+        public const string RANGO_OPCIONES = "Elige una opcion entre 1 y 5";
+        public const string OPCION_MENU = "Ingresa una opción del menú: ";
+        public const string NO_SELECCION_IRIS = "No se ha ingresado los datos de la iris uno u iris dos";
+        public const string ERROR_INGRESAR_NUMERO = "Error al ingresa el número, vuelve a intentarlo";
+        public const string ERROR_NUMERO_GRANDE = "Número demasiado grande, intenta con otro número";
+        public const string ERROR_INGRESAR_STRING_VACIO = "El string se encuentra vacío, vuelve a intentarlo";
+        public const string ERROR_INGRESAR_STRING_VACIO_ESPACIOS = "Ingresa un string sin espacios";
+        public const string ERROR_INGRESAR_STRING_GRANDE = "Error al ingresar el texto, vuelve a intentarlo";
+        public const string TEXTURA = "Ingresa la textura de la iris: ";
+        public const string FORMA = "Ingresa la forma de la iris: ";
+        public const string COLOR = "Ingresa el color de la iris: ";
+        public const string INTENSIDAD = "Ingresa la intensidad de la iris: ";
+        public const string PATRON = "Ingresa el patron de la iris: ";
+        public const string CURVATURA = "Ingresar la curvatura de la iris: ";
+        public const string OTROS = "Ingresa si la iris tiene manchas o cicatrices: ";
+        public const string OPCIONES_MENU = "1. Ingresar la información del primer Iris. \n2. Ingresar la información del segundo Iris.\n3. Autentificación\n4. Simular envejecimiento\n5. Salir";
+        public const string OPCIONES_IRIS = "1. Iris Uno\n2. Iris dos";
+        public const string OPCIONES_ENVEJECIMIENTO = "1. Intensidad del color\n2. Cambiar forma del iris (curvatura)\n3. Añadir manchas y cicatrices";
+        public const string NO_OPCION_MENU = "No es una opción del menú";
+        public const string SELECCION_IRIS = "Selecciona una iris de la lista: ";
+        public const string RANGO_OPCIONES_SUBMENU = "Elige una opcion entre 1 y 3";
+        public const string NO_SELECCION_SUBMENU = "No se ha selecciono una opción del menú";
+        public const string INGRESO_INTENSIDAD = "Ingresa la intensidad del color del iris: ";
+        public const string INGRESO_CURVATURA = "Ingresa la curvatura del iris: ";
+        public const string INGRESO_OTRO = "Ingresa la cicatriz o mancha del iris: ";
+        //Método que concatena la opción del menú seleccionado
+        public static string OpcionMenu(int valor)
+        {
+            return string.Format("Has elegido la opción {0}", valor);
+        }
+    }
+}
