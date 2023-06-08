a53a9f0997e5b7200d17f7716d02281809ddcc7f
Raul Ramirez Perez <ramssrez@gmail.com>
2023-06-08 20:43:48 +0200

    Cración del a actividad número 3 de la unidad

---
+++ ProgramacionNetII/DPRN2_U3_A3_RARP/Empleado.cs
@@ -0,0 +1,25 @@
+﻿namespace UnidadTresA3
+{
+    public class Empleado
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public decimal Salario { get; set; }
+        public Empleado()
+        {
+
+        }
+        public Empleado(int codigo, string nombre, decimal salario)
+        {
+            Codigo = codigo;
+            Nombre = nombre;
+            Salario = salario;
+        }
+        public string DatosEntidad()
+        {
+            return $"Codigo: {Codigo}\n" +
+                    $"Nombre: {Nombre}\n" +
+                    $"Salario: {Salario}";
+        }
+    }
+}
---
+++ ProgramacionNetII/DPRN2_U3_A3_RARP/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace UnidadTresA3
+{
+    class Program
+    {
+        //Declaración de la variable global de la aplicación
+        //public static EntidadesMetaverso entidadesMeta = new();
+        //public static UsuarioMetaverso usuario = new("Gratuita");
+        public static List<Empleado> empleados = new List<Empleado>();
+        //Comienzo de la función principal en C#
+        static void Main(string[] args)
+        {
+            //Inicio del método que contiene el menú de principal.
+            Presentacion();
+        }
+        //Método que muesta el menú de opciones para el caso de estudio
+        public static void Presentacion()
+        {
+            Empleado empleado = new();
+            Console.WriteLine(empleados.Count);
+
+        }
+    }
+}
