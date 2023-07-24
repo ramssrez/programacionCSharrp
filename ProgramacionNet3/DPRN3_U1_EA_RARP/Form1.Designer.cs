4194eb70ed4ec20ad30c547aa9f99d40760d6d1a
Raul Ramirez Perez <ramssrez@gmail.com>
2023-07-24 14:04:08 +0200

    Instalación de la librería y primera conexión con la base de datos

--- ProgramacionNet3/DPRN3_U1_EA_RARP/Form1.Designer.cs
+++ ProgramacionNet3/DPRN3_U1_EA_RARP/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnProveedor = new System.Windows.Forms.Button();
+            this.dgvProvedores = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProvedores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnProveedor
+            // 
+            this.btnProveedor.Location = new System.Drawing.Point(301, 28);
+            this.btnProveedor.Name = "btnProveedor";
+            this.btnProveedor.Size = new System.Drawing.Size(181, 47);
+            this.btnProveedor.TabIndex = 0;
+            this.btnProveedor.Text = "Obtener Provedores";
+            this.btnProveedor.UseVisualStyleBackColor = true;
+            this.btnProveedor.Click += new System.EventHandler(this.btnProveedor_Click);
+            // 
+            // dgvProvedores
+            // 
+            this.dgvProvedores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProvedores.Location = new System.Drawing.Point(12, 81);
+            this.dgvProvedores.Name = "dgvProvedores";
+            this.dgvProvedores.RowHeadersWidth = 51;
+            this.dgvProvedores.RowTemplate.Height = 24;
+            this.dgvProvedores.Size = new System.Drawing.Size(762, 158);
+            this.dgvProvedores.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvProvedores);
+            this.Controls.Add(this.btnProveedor);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProvedores)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnProveedor;
+        private System.Windows.Forms.DataGridView dgvProvedores;
     }
 }
 
--- ProgramacionNet3/DPRN3_U1_EA_RARP/Form1.cs
+++ ProgramacionNet3/DPRN3_U1_EA_RARP/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DPRN3_U1_EA_RARP
 {
@@ -15,6 +16,39 @@ namespace DPRN3_U1_EA_RARP
         public Form1()
         {
             InitializeComponent();
+        }
+        SqlConnection RRPSqlConnection;
+        SqlDataAdapter RRPDataAdapter;
+        DataSet RRPDataSet;
+        string cadenaConexion = "Data Source = LAPTOP-AVRBQCPP\\SQLEXPRESS; Initial Catalog = RRPdbProveedores; Integrated Security = True";
+
+        private void btnProveedor_Click(object sender, EventArgs e)
+        {
+            try{
+                using (RRPSqlConnection = new SqlConnection(cadenaConexion))
+                {
+                    RRPSqlConnection.Open();
+                    ConsultaProvedores();
+                    RRPSqlConnection.Close();
+                }
+            }
+            catch(Exception ex){
+                MessageBox.Show("No se puede conectar a la base de datos" + ex.Message);
+            }
+        }
+
+        private void ConsultaProvedores()
+        {
+            RRPDataAdapter = new SqlDataAdapter("select * from RRPProvedores", RRPSqlConnection);
+            RRPDataSet = new DataSet();
+            RRPDataAdapter.Fill(RRPDataSet,"proveedores");
+            dgvProvedores.DataSource = RRPDataSet;
+            dgvProvedores.DataMember = "proveedores";
+            /*
+            dgvProvedores.Columns["Id_proveedor"].HeaderText = "Id";
+            dgvProvedores.Columns["Nombre"].HeaderText = "Nombre";
+            dgvProvedores.Columns["ApellidoPaterno"].HeaderText = "Descripcion";
+            */
         }
     }
 }
