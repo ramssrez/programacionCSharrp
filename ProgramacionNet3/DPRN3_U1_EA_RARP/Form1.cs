4dbd831f2b990d2d79c24d52d02ba9848ad7038f
Raul Ramirez Perez <ramssrez@gmail.com>
2023-07-24 13:36:48 +0200

    Creación de la evidencia de aprendizaje

---
+++ ProgramacionNet3/DPRN3_U1_EA_RARP/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DPRN3_U1_EA_RARP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
