d5a60cfa5ce24d61f1274dcbff4cea177eac3d84
Raul Ramirez Perez <ramssrez@gmail.com>
2023-09-11 12:10:20 +0200

    Documentación de las clases de medicos, especialidad y paciente

--- ProgramacionNet3/DSDPRN3RRP2302B1/Especialidad.cs
+++ ProgramacionNet3/DSDPRN3RRP2302B1/Especialidad.cs
@@ -1,7 +1,9 @@
 ﻿namespace DSDPRN3RRP2302B1
 {
+    //Clase para poder realizar la creación de una especialidad con diferentes atributos
     internal class Especialidad
     {
+        //Atributos de la clase con sus repectivos getters y settersgit 
         public int IdRRP { get; set; }
         public string NombreRRP { get; set; }
         public string DescripcionRRP { get; set; }
--- ProgramacionNet3/DSDPRN3RRP2302B1/Paciente.cs
+++ ProgramacionNet3/DSDPRN3RRP2302B1/Paciente.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace DSDPRN3RRP2302B1
 {
+    //Clase para poder crear objetos de tipo Paciente
     internal class Paciente
     {
+        //Atributos de la clase con sus repectivos getters y setters
         public int IdRRP { get; set; }
         public string NombreRRP { get; set; }
         public string ApellidoPaternoRRP { get; set; }
